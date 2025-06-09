0091ebc83c2ed1a87497544ee6174403859019ce
Felix Angga Resky <151905402+FelixStillLearning@users.noreply.github.com>
2025-06-09 15:19:25 +0700

    npm start

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,10 @@
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize('db_tiketgercep', 'root', '', {
-    host: 'localhost',
+const db = new Sequelize(
+    process.env.DB_NAME || 'db_tiketgercep', 
+    process.env.DB_USER || 'root', 
+    process.env.DB_PASS || '', {
+    host: process.env.DB_HOST || 'localhost',
     dialect: 'mysql'
 });
 
